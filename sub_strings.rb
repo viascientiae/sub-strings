4789cf0a5e2d57c083322c0a961bed75c4d3f065
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-10-05 18:20:59 +0530

    Add logic for finding substrings in original word

--- sub_strings.rb
+++ sub_strings.rb
@@ -2,4 +2,25 @@ def substrings(word, substrings_array)
 
   hash_listing = {}
 
+  substrings_array.each do |substring|
+
+    if word.include?(substring)
+     
+      if hash_listing[substring]
+        hash_listing[substring] += 1      
+      elsif
+        hash_listing[substring] = 0
+        hash_listing[substring] += 1
+      end
+     
+    end
+
+  end
+
+puts hash_listing
+
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("below", dictionary)
